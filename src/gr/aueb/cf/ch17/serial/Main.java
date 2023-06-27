c9bf4479347c54183be4c8794fcc47742f194e83
stavrosnaz <stavrosnaz@gmail.com>
2023-06-27 21:02:51 +0300

    27/06/23

---
+++ src/gr/aueb/cf/ch17/serial/Main.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.ch17.serial;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("C:/tmp/serial216.ser");
+        Student student = new Student(1, "ALice", "W.");
+        writeObj(student, file);
+
+        Student studentToRead;
+        studentToRead = readObj(file);
+
+    }
+
+    public static void writeObj(Student stu, File file) {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(stu);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Student readObj (File file) {
+        Student student = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            student = (Student) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return student;
+    }
+}
---
+++ src/gr/aueb/cf/ch19/NodeGenericApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch19;
+
+public class NodeGenericApp {
+
+    public static void main(String[] args) {
+        NodeGeneric<Integer> nodeInt = new NodeGeneric<>();
+        NodeGeneric<String> nodeStr = new NodeGeneric<>();
+
+        nodeInt.setValue(3);
+    }
+}
